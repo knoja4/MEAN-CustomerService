a2496f8d187ff2ba1cc763c8c78f353aa6916db3
Noah Coughlan <noah@COUGHLAN-UBUNTU.attlocal.net>
2017-03-30 15:55:40 -0700

    OK

--- controllers/api/customers/customers.controller.js
+++ controllers/api/customers/customers.controller.js
@@ -3,7 +3,7 @@ const customersRepo = require('../../../lib/customersRepository'),
       util = require('util');
 
 class CustomersController {
-
+    //  /api/customers
     constructor(router) {
         router.get('/', this.getCustomers.bind(this));
         router.get('/page/:skip/:top', this.getCustomersPage.bind(this));
